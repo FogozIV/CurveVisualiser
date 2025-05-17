40943ddd77d5e59b3a63562d452de811f31f2161
FogozIV <fogoziv@gmail.com>
2025-05-17 13:45:58 +0200

    Added files

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#ifdef __MINGW32__
+#define _POSIX_C_SOURCE 200809L
+#include <cmath>
+#undef _POSIX_C_SOURCE
+
+// Workaround for Python 3.12+ on MinGW
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#endif
+#include "matplotlibcpp.h"
+#include <iostream>
+
+#include "Visualizer.h"
+#include "curves/BezierCurve.h"
+#include "curves/ClothoidCurve.h"
+#include "utils/G2Solve3Arc.h"
+#include "utils/Position.h"
+
+namespace plt = matplotlibcpp;
+int main() {
+    Position start(0.0, 0.0, Angle::fromDegrees(0), 0);
+    Position end(55, 100, Angle::fromDegrees(180), 0);
+    Position end2(-200, 450, Angle::fromDegrees(180), 0.02);
+    Position end3(1000, 450, Angle::fromDegrees(180), 0);
+    Position end4(1200, 800, Angle::fromDegrees(180), 0);
+    G2Solve3Arc arc;
+    std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+    int sum = arc.build(start, end);
+    sum += arc.build(end, end2);
+    sum += arc.build(end3, end2);
+    sum += arc.build(end3, end4);
+    std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+    std::cout << "Time elapsed: " << elapsed_seconds.count() << "s" << std::endl;
+    std::cout << sum << std::endl;
+
+
+
+    std::vector<std::shared_ptr<BaseCurve>> curves = {};
+    std::cout << arc.build(start, end) << std::endl;
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment0()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegmentMiddle()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment1()));
+    std::cout << arc.build(end, end2) << std::endl;
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment0()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegmentMiddle()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment1()));
+    std::cout << arc.build(end3, end2) << std::endl;
+    arc.reverse();
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment0()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegmentMiddle()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment1()));
+    std::cout << arc.build(end3, end4) << std::endl;
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment0()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegmentMiddle()));
+    curves.push_back(std::make_shared<ClothoidCurve>(arc.getSegment1()));
+
+    CurveVisualizer::plotCurve(curves, 100, true);
+    CurveVisualizer::plotCurvatureEvolution(curves, 100, true);
+
+    return 0;
+}
